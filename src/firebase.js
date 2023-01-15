3ed719b29570e355952cf9518d02412c17a07b1a
Deepak <dbehera647@gmail.com>
2023-01-15 15:48:59 +0530

    adding data to the cloud firestore

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2DxswpvpXfBzgUDILAZgWNKxHt8lShac",
-    authDomain: "react-hooks-blog-c2172.firebaseapp.com",
-    projectId: "react-hooks-blog-c2172",
-    storageBucket: "react-hooks-blog-c2172.appspot.com",
-    messagingSenderId: "172776956427",
-    appId: "1:172776956427:web:89a99d9e39408909251d0e"
+  apiKey: "AIzaSyA2DxswpvpXfBzgUDILAZgWNKxHt8lShac",
+  authDomain: "react-hooks-blog-c2172.firebaseapp.com",
+  projectId: "react-hooks-blog-c2172",
+  storageBucket: "react-hooks-blog-c2172.appspot.com",
+  messagingSenderId: "172776956427",
+  appId: "1:172776956427:web:89a99d9e39408909251d0e"
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
